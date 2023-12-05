6024c7fa75dcbe4f0c4a12e85565630af7efd3b7
Milene Fukuda <milene.fukuda@gmail.com>
2023-12-05 20:19:46 +0000

    primeira planilha excel com python

---
+++ app.py
@@ -0,0 +1,22 @@
+import openpyxl
+
+# Criar uma planilha (salvar na variável book)
+book = openpyxl.Workbook()
+
+# Como visualizar páginas existentes
+print(book.sheetnames)
+
+# Criar uma página
+book.create_sheet('Frutas')
+
+# Como selecionar uma página
+frutas_page = book['Frutas']
+
+# Como criar colunas e linhas
+frutas_page.append(['Fruta', ' Quantidade', 'Preço'])
+frutas_page.append(['Banana', ' 9', '0,99'])
+frutas_page.append(['Maça', ' 7', '9,00'])
+
+# Salvar a planilha
+book.save('Planilha de Compras.xlsx')
+
